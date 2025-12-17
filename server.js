24eb027a5c2aa9afeeb632e6ea34a76445f6d1b5
ngbaokiet223 <ngbaokiet223@gmail.com>
2025-12-17 11:12:54 +0700

    Khoi tao du an

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Xin chao STU - Project Nodejs</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`App dang chay tai port ${port}`);
+});
