b426569482d6c0cebb7c475488ae67cea862eaa0
ngbaokiet223 <ngbaokiet223@gmail.com>
2025-12-18 15:26:54 +0700

    Update Project 1: Them thong tin sinh vien

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Xin chao STU - Phien ban FEATURE</h1>');
+  res.send(`
+        <h1 style="color: red; text-align: center;">Thi Thứ Năm - Ca 4 - Nguyễn Bảo Kiệt</h1>
+        <h2 style="text-align: center;">Project 1: Node.js Backend trên Render</h2>
+        <p style="text-align: center;">Demo CI/CD tự động thành công!</p>
+    `);
 });
 
 app.listen(port, () => {
